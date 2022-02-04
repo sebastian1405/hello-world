1a335b4ed77766f21bd2e4f85fcbadc22f50bdd6
sebastian guevara <sebastianguevara32569830@gmail.com>
2022-02-04 18:21:30 -0300

    terminado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect , useState} from 'react';
 
 function App() {
+  const [saludo, setSaludo ] = useState ('')
+  useEffect ( () => {
+    setSaludo('¡Hello World! 😱')
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello World App</h1>
+      <p>{saludo}</p>
     </div>
   );
 }
